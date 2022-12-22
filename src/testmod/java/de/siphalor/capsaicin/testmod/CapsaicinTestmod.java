7a5c8c64fe2fed1994a79e2bb0789aa0fef7e922
Siphalor <info@siphalor.de>
2022-12-22 01:42:17 +0100

    Add a testmod

---
+++ src/testmod/java/de/siphalor/capsaicin/testmod/CapsaicinTestmod.java
@@ -0,0 +1,28 @@
+package de.siphalor.capsaicin.testmod;
+
+import com.mojang.datafixers.util.Pair;
+import de.siphalor.capsaicin.api.food.FoodModificationRegistry;
+import de.siphalor.capsaicin.api.food.PlayerFoodModifier;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+
+public class CapsaicinTestmod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		FoodModificationRegistry.EATING_TIME_MODIFIERS.register((PlayerFoodModifier<Integer>) (eatingTime, context, player) -> {
+			float hungerRatio = 1F + (20 - player.getHungerManager().getFoodLevel()) / 20F;
+			return (int) (eatingTime * hungerRatio);
+		}, new Identifier("capsaicin-testmod", "test"));
+		FoodModificationRegistry.PROPERTIES_MODIFIERS.register((foodProperties, context) -> {
+			if (context.getStack().getItem() == Items.COD) {
+				foodProperties.getStatusEffects().add(Pair.of(new StatusEffectInstance(StatusEffects.POISON, 20 * 5, 2), 1F));
+			} else if (context.getStack().getItem() == Items.COOKIE) {
+				foodProperties.setAlwaysEdible(true);
+			}
+			return foodProperties;
+		}, new Identifier("capsaicin-testmod", "test"));
+	}
+}
